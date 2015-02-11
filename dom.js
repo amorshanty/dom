0face63f570cb887395b9fa6ce480c975da7cb6a
Amor <amor.dibella@gmail.com>
2015-02-11 18:37:39 +0100

    first commit

---
+++ dom.js
@@ -0,0 +1,79 @@
+$(document).ready(function () {
+
+// Change the width of the h1 element
+	$("h1").width('760px')
+
+// Increase the font-size of all elements to 120%
+	$("*").css('font-size' , "100%")
+
+// Add the hightlighted class to the elements that has the 'new' class
+	$('.new').addClass('hightlighted')
+
+// Remove the title class from h1
+	$('h1').removeClass("title")
+
+// Set the default age range in 20-30
+	$('#age-range').val('20-30')
+	
+
+// Set the default age range in 20-30 using travesing filters
+	.removeClass("starred")
+
+
+// Uncheck the "I love learning" checkbox
+		$('#love').prop("selected", false)
+
+// Select yellow as a favourite color
+		$("#color-yellow").prop("checked", "checked")
+
+
+// Write the selected name of the input in the result div
+		$('result').text($('name').val())
+
+
+// Write the text of the selected age in the result div
+
+
+
+// Writes the number of data likes of jquery-ui in the result div
+
+
+
+// Increase in 100 the number of likes and show the number of likes in the result div
+
+
+// Show the absolute position of the result div in the console
+
+
+// Move the result div to the position 100 top and 150 left
+
+
+// Change the width and height of the result up to 300px
+
+
+// Add " (User interface)" after "JQueryUI"
+
+
+// Add a li element with the text "Future JQ" between jQueryUI and jQueryMobile
+
+
+// Add div containers with the class bordered around every li
+
+
+// Add a div container with the class bordered around all li
+
+
+// Remove the result div
+
+
+    /**
+     *  Final round
+     */
+
+// Set the font color of the color names according to their color
+// Example: The red text should be in red, the blue text should be in blue...
+
+
+// Add links to the li elements, the url is the name ended with .com
+
+});
